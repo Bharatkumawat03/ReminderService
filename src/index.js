dac1600335253a3c2c50074a7dcd23b00c017508
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-10-01 15:57:20 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const {PORT}=require('./config/serverConfig');
+const { sendBaiscEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
 
@@ -12,6 +13,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`server started at PORT ${PORT}`);
+
+        sendBaiscEmail(
+            'support@admin.com',
+            'potter0368harry@gmail.com',
+            'this is a testing email',
+            'hey, how are you, i hope you like the support'
+        );
     });
 }
 
