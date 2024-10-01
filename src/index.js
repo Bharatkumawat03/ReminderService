1480471515e3a7e73d9940ffb73a2b54f1e6083d
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-10-01 23:43:38 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 const {PORT}=require('./config/serverConfig');
-const { sendBaiscEmail } = require('./services/email-service');
+// const { sendBaiscEmail } = require('./services/email-service');
+const cron = require('node-cron');
 
 const setupAndStartServer = () => {
 
@@ -14,12 +15,16 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`server started at PORT ${PORT}`);
 
-        sendBaiscEmail(
-            'support@admin.com',
-            'potter0368harry@gmail.com',
-            'this is a testing email',
-            'hey, how are you, i hope you like the support'
-        );
+        // sendBaiscEmail(
+        //     'support@admin.com',
+        //     'potter0368harry@gmail.com',
+        //     'this is a testing email',
+        //     'hey, how are you, i hope you like the support'
+        // );
+
+        cron.schedule('* * * * *', () => {
+            console.log('running a task every minute');
+          });
     });
 }
 
