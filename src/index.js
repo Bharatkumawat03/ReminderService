0df4121c99c98b7d49a78f6f83f02d99414aca22
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-10-04 01:21:11 +0530

    complete message queue inter service communication

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const {PORT}=require('./config/serverConfig');
+const {PORT, REMINDER_BINDING_KEY}=require('./config/serverConfig');
+const { createChannel, subscribeMessage } = require('./utils/messageQueue');
 // const { sendBaiscEmail } = require('./services/email-service');
 // const cron = require('node-cron');
 
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
 
-const setupAndStartServer = () => {
+
+const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.post('/api/v1/tickets', TicketController.create);
     
 
     app.listen(PORT, () => {
         console.log(`server started at PORT ${PORT}`);
-        jobs(); 
+        // jobs(); 
 
         // sendBaiscEmail(
         //     'support@admin.com',
